bffd8f0245e211c9ffcaa3d99b7b6c5bae071aee
Cheryet <C.heryet@outlook.com>
2022-11-20 09:24:32 -0700

    add route file for server.js

---
+++ routes/login.js
@@ -0,0 +1,9 @@
+// Takes user to login page to
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+module.exports = router;
