abff7687069e00d2061ce8ff8fd7af9384053ccc
Ben Stolb <benstolb@protonmail.com>
2022-11-20 18:01:56 -0800

    Add users db seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, password, phone) VALUES ('Alice', 'password', '777-777-7777');
+
