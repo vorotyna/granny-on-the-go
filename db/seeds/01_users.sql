cab2cdb799774b75e76cbbd403118a68cbfc85a9
Ben Stolb <benstolb@protonmail.com>
2022-11-20 18:14:08 -0800

    Add orders db seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, password, phone) VALUES ('Alice', 'password', '777-777-7777');
+INSERT INTO users (name, password, phone) VALUES (
+'Alice',
+'password',
+'777-777-7777');
 
---
+++ db/seeds/03_orders.sql
@@ -0,0 +1,5 @@
+INSERT INTO orders (time_placed, is_accepted, aprox_time, total_price, notes) VALUES (
+  "2022-11-20 6:11:11",
+  TRUE,
+  45,
+  'Please add triple the amount of pickles in the lasagna');
