57ce1aaabe113cb56782e66cc5d21ae8421412d1
Ben Stolb <benstolb@protonmail.com>
2022-11-20 18:25:52 -0800

    Add items db seed

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,5 +1,7 @@
-INSERT INTO orders (time_placed, is_accepted, aprox_time, total_price, notes) VALUES (
+INSERT INTO orders (time_placed, is_accepted, aprox_time, total_price, notes, restaurant_id, user_id) VALUES (
   "2022-11-20 6:11:11",
   TRUE,
   45,
-  'Please add triple the amount of pickles in the lasagna');
+  'Please add triple the amount of pickles in the lasagna',
+  1,
+  1);
