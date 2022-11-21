84a9c8f152a9a7507764766b0c06cba1976c4ee0
Ben Stolb <benstolb@protonmail.com>
2022-11-20 18:07:19 -0800

    Add restaurants db seed

---
+++ db/seeds/02_restaurants.sql
@@ -0,0 +1,1 @@
+INSERT INTO restaurants (name, description, phone, profile_img_url) VALUES ("Nonna Maria", 'This is a description. It describes things like myself and my food! Wow! Eat here!', '.../public/images/granny.png');
