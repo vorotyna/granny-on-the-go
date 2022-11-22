84a1d12f44aba908d8d82eaff113ba741ecca691
Cheryet <C.heryet@outlook.com>
2022-11-22 08:26:29 -0700

    add coolie session dependancy

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,14 @@
 // Takes user to login page to
+var cookieSession = require('cookie-session')
 const express = require('express');
 const router  = express.Router();
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
+
 
 router.get('/', (req, res) => {
   res.render('login');
