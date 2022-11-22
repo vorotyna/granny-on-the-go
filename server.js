abb014a8f4614f1675c3d87fef5bf8f3ea41cdc5
Cheryet <C.heryet@outlook.com>
2022-11-22 09:49:42 -0700

    add logout route and update home page route logic

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
 
-
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+
+const cookieSession = require('cookie-session')
+
 app.set('view engine', 'ejs');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -33,6 +39,8 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
+const loginPostRoutes = require('./routes/login-post')
+const logoutRoutes = require('./routes/logout')
 const myOrderRoutes = require('./routes/my-order')
 const restaurantRoutes = require('./routes/start-order')
 const myOrderCustomerApiRoutes = require('./routes/sms-api-customer')
@@ -46,6 +54,8 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/login', loginRoutes )
+app.use('/login', loginPostRoutes)
+app.use('/logout', logoutRoutes)
 app.use('/my-order', myOrderRoutes)
 app.use('/restaurant', restaurantRoutes)
 app.use('/my-order/customer', myOrderCustomerApiRoutes)
@@ -63,7 +73,14 @@ app.use('/my-order/restaurant', myOrderRestaurantApiRoutes)
 
 
 app.get('/', (req, res) => {
-  res.render('index');
+
+  if (!req.session.id){
+    const templateVars = {user: false}
+    res.render('index', templateVars)
+  }
+
+  const templateVars = {user: true}
+  res.render('index', templateVars);
 });
 
 app.listen(PORT, () => {
