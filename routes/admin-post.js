32b23aeaf480994a2e062f8d085012f55af2627b
Cheryet <C.heryet@outlook.com>
2022-11-24 13:32:27 -0700

    complete routing

--- routes/admin-post.js
+++ routes/admin-post.js
@@ -6,8 +6,19 @@ const app = express();
 
 router.post('/' , (req,res) => {
 
-  const templateVars = {time: req.body.time}
-  res.render('order-complete', templateVars)
+  if (!req.session.id){
+    const templateVars = {user: false, admin: false}
+    res.render('login', templateVars)
+  }
+
+  if (req.session.id === 'admin'){
+    const templateVars = {user: true, admin: true, time:req.body.time}
+    return res.render('order-complete', templateVars)
+  }
+
+  const templateVars = {user: true, admin: false}
+  res.render('login', templateVars);
+
 })
 
 module.exports = router;
